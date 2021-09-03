a7c6d4103faa5ee8726153b8b840a09de8f5b291
Patrik-cosmic <sumande2019@outlook.com>
2021-09-03 20:05:22 +0530

    You are good

--- extra.py
+++ extra.py
@@ -1,5 +1,5 @@
-import keyword
-import re
+# import keyword
+# import re
 # print(keyword.kwlist)
 # num = 10
 # num = int(10)
@@ -7,4 +7,27 @@ import re
 # print(type(num))
 # 'bdfjgjdfgdfhgdfng4649+'
 
+# some extra stuff ...
+
+# def test():
+#     return 101
+# 
+# 
+# result = print('odd') # unknown
+# result2 = test() # 101
+# 
+# print(result)
+# print(result2)
+
+
+
+
+num = 45
+
+print(id(num))
+
+if num%2 == 0:
+    print('Even')
+else:
+    print('Odd')
 
