7cac0d648e8705be78cbe64b07eb8ce742efc798
Patrik-cosmic <sumande2019@outlook.com>
2021-09-04 20:17:42 +0530

    You are good

---
+++ container.py
@@ -0,0 +1,54 @@
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+# print(type(numbers))
+# print(numbers)
+
+# result = numbers.append(9)
+# result2 = numbers.count(3)
+# removed_item = numbers.pop() # 8
+
+# print(removed_item)
+# print(numbers)
+
+
+# print(numbers)
+# print(result)
+# print(result2)
+
+# tuple 
+#days = ('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat')
+
+
+# days_list = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+# days_list.append('Spider man')
+# # days.append('Spider')
+# print(days_list)
+
+# days = (
+#     'sun', 
+#     'mon', 
+#     'Tue',
+#     'Wed', 
+#     'Thu', 
+#     'Fri',
+#     'Fri',
+#     True,
+#     [1, 2, 3, 4, 5, 6, 7, 8], # add number 9
+#     (True, False, True)
+# )
+
+# days[8][4] = 99
+print(days)
+
+# print(days.count('Fri'))
+# position = days.index([1, 2, 3, 4, 5, 6, 7, 8])
+# print(position)
+
+# # print(days[7])
+# # print(type(days[7]))
+
+# days[7].append(9)
+
+# print(days)
+# print(days[7])
