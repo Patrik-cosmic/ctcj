4fd956176426faf269a05775cb1c832094856887
Patrik-cosmic <sumande2019@outlook.com>
2021-09-12 11:37:11 +0530

    You are good

--- revise.py
+++ revise.py
@@ -1,5 +1,5 @@
 # revise again and again 
-# def add(*numbers):
+# def add(*numbers): # *args
 #     print(type(numbers))
 #     return sum(numbers)
 
@@ -20,3 +20,39 @@
 # print(add(*all_numbers))
 
 # print(sum(all_numbers, 3))
+
+
+
+# def info_student(name, roll, section):
+#     print(name, roll, section, sep=' -> ')
+
+# def info_employee(name, id, dept, tech, salary, exp):
+#    pass
+#    # or
+#    ...
+
+# info_student('Suman De', 2, 'M.Sc.')
+
+# def info(**attrs): #**kwargs
+#     print(type(attrs), end='\n\n')
+#     print(attrs, end='\n\n')
+#     print(attrs.keys(), end='\n\n')
+#     print(attrs.values(), end='\n\n')
+#     print(attrs.items(), end='\n\n')
+
+#     # emp specific condition 
+#     if 'tech' in attrs:
+#         print(attrs['tech'])
+#     else:
+#         print("The key tech is not present \n")
+
+
+# info(name='Suman De', id = 2, dept = 'M.Sc.')
+# info(name='Patrik Dark', id=102, dept='Full stack', tech=['Python','Django','React','PGSQL'], salary=9999999, exp=6)
+
+# def func_name(*args, **kwargs):
+#     print(args)
+#     print(kwargs)
+
+# func_name(1, 2, 3, 4, 5, name = 'Suman De', computer = 'Laptop')
+# func_name(1, 2, 3, 4, 5, name = 'Suman De', computer = 'Laptop', 9) # home task about this error
