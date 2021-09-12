5ff5635432a51f7451cf4df2d1ddf9c94235ce49
Patrik-cosmic <sumande2019@outlook.com>
2021-09-12 10:53:57 +0530

    You are good

--- revise.py
+++ revise.py
@@ -1,1 +1,22 @@
 # revise again and again 
+# def add(*numbers):
+#     print(type(numbers))
+#     return sum(numbers)
+
+    # sum = 0
+    # for num in numbers:
+    #     sum = sum + num
+    # return sum
+
+# print(add())
+# print(add(2))
+# print(add(2,5))
+# print(add(2,3,5))
+
+# all_numbers = [1, 4, 5, 6, 7]
+# print(all_numbers, sep='\t')
+# print(*all_numbers, sep='\t')
+
+# print(add(*all_numbers))
+
+# print(sum(all_numbers, 3))
